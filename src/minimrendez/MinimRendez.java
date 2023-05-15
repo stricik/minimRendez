98a8cddd05885ed580b9740f5319b759335cb6b7
Szendefi Krisztofer Dániel (SZF_2022_2024) <szendefi.krisztofer.daniel@diak.szamalk-szalezi.hu>
2023-05-15 12:30:15 +0200

    randomFeltoltes()

--- src/minimrendez/MinimRendez.java
+++ src/minimrendez/MinimRendez.java
@@ -1,23 +1,39 @@
+    package minimrendez;
 
-package minimrendez;
+import java.util.Random;
 
 public class MinimRendez {
 
+    static Random rnd = new Random();
+    
     public static void main(String[] args) {
-        int[] tomb = {
-            334, 434, 2, 34, 312, 31,6 ,56 , 654, 33
-        };
+        int[] tomb = new int [10];
+        tomb = randomFeltoltes(tomb, 10);
+        System.out.println("rendezetlen:");
+        tombKiir(tomb);
         int[] rendezett = minimumRendezes(tomb);
+        System.out.println("rendezett:");
+        tombKiir(rendezett);
+    }
+
+    private static int[] randomFeltoltes(int[] tomb, int szam) {
+        for (int i = 0; i < szam; i++) {
+            tomb[i] = rnd.nextInt(100);
+        }
+        return tomb;
+    }
+
+    private static void tombKiir(int[] rendezett) {
         for (int i = 0; i < rendezett.length - 1; i++) {
             System.out.print(rendezett[i] + ", ");
         }
-        System.out.print(rendezett[rendezett.length-1]);
+        System.out.println(rendezett[rendezett.length - 1]);
     }
 
     private static int[] minimumRendezes(int[] tomb) {
         for (int i = 0; i < tomb.length - 1; i++) {
             int min = i;
-            for (int j = i+1; j < tomb.length; j++) {
+            for (int j = i + 1; j < tomb.length; j++) {
                 if (tomb[min] > tomb[j]) {
                     min = j;
                 }
@@ -29,5 +45,5 @@ public class MinimRendez {
         }
         return tomb;
     }
-    
+
 }
